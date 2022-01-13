01c8a7be3c9a96cb15ecd642486f380bb1766b72
C. Carouge <c.carouge@unsw.edu.au>
2022-01-13 17:21:56 +1100

    Add time period selection

--- cafaz/cafaz.py
+++ cafaz/cafaz.py
@@ -21,6 +21,8 @@ class Cafaz(object):
         self.root = Path("cafe60-reanalysis-dataset-aws-open-data")
         self.varname = varname
         self.realm = realm
+        if time_res == "monthly":
+            time_res = "month"
         self.time_res = time_res
         self.json_root = Path(json_root)
 
@@ -64,13 +66,73 @@ class Cafaz(object):
             *[dask.delayed(self.gen_json)(u, json_path) for u in urls], retries=10
         )
 
-    def read_CAFE(self):
-        """Read the full timeseries for the variable defined"""
+    @staticmethod
+    def date_to_YYYYmmdd(period):
+        """Convert a period given as a slice to [YYYYmm01, YYYYmm01] in integers
+        period: slice, dates are in the format 'YYYY' or 'YYYY-mm'."""
+
+        converted = [period.start, period.stop]
+        for pos, date in enumerate(converted):
+            date_split = date.split("-")
+
+            assert (
+                len(date_split) <= 2 and len(date_split) >= 0
+            ), "Wrong format provided for the period"
+
+            if len(date_split) == 1:  # Only the year is provided
+                year = (
+                    int(date_split[0]) + pos
+                ) * 10000  # Add one to the year of the end date to take the whole year.
+                converted[pos] = year + 101  # Add "0101" in integer form
+            else:  # YYYY-mm
+                year = (
+                    int(date_split[0]) + pos
+                ) * 10000  # Add one to the year of the end date to take the whole year.
+                month = (
+                    int(date_split[1]) + pos
+                ) * 100  # Add one to the month of the end date to take the whole month
+                converted[pos] = year + month + 1
+
+        return converted
+
+    @staticmethod
+    def select_time(full_list, period=None):
+        """Restrict a list of file names to those covering the required period.
+        fulllist: list, sorted list of Path variables
+        period: slice, time period to use. If provided, use the format:
+                YYYY or YYYY-mm or YYYY-mm-dd."""
+
+        loc_list = full_list.copy()
+
+        # Only modify the list if period is provided.
+        # Assumes the CAFE60 naming convention for the filenames:
+        # <var>.<realm>.<time res>.CAFE60.<YYYYmmdd>-<YYYYmmdd>.nc
+        if period:
+            # Convert the start and end times of the period to
+            # the same format as in CAFE filenames
+            start_period, end_period = Cafaz.date_to_YYYYmmdd(period)
+
+            new_list = []
+            for ind_file, my_path in enumerate(loc_list):
+                file_dates = my_path.stem.split(".")[4]
+                file_start = int(file_dates.split("-")[0])
+                file_end = int(file_dates.split("-")[1])
+                if file_start >= start_period and file_end <= end_period:
+                    new_list.append(my_path)
+
+        return new_list
+
+    def read_CAFE(self, period=None):
+        """Read the full timeseries for the variable defined
+        period: slice, time period to read. If not provided, it will read the whole time period.
+                Dates should be given as: 'YYYY' or 'YYYY-mm'"""
 
         json_path = self.json_root / self.realm
         json_list = sorted(
             json_path.glob(f"{self.varname}.{self.realm}.{self.time_res}*.json")
         )
+
+        json_list = self.select_time(json_list, period)
         m_list = []
         for js in json_list:
             with open(js) as f:
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="cafaz",
-    version="0.1.0",
+    version="0.2.0",
     description="To read CAFE60 on AWS from NCI",
     url="https://github.com/coecms/CAFEAWS",
     author="Claire Carouge",
